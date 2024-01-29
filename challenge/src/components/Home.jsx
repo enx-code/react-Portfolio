002763d9e3910e09574b2feb0f001c6476c9a129
Enky <enkh6at@yahoo.com>
2024-01-29 11:04:55 +0000

    App.jsx file route created

---
+++ challenge/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, hic corporis natus sed quae doloremque repellat, neque laudantium quia tempora minus, quam molestias. Facilis nostrum vitae deserunt adipisci reprehenderit provident!</div>;
+};
+
+export default Home;
