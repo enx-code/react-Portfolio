83b1d8f5b2f8382326de546e40ccd7cb18f0f137
Enky <enkh6at@yahoo.com>
2024-03-07 11:18:08 +0000

    adding booystrap in project and work file

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,18 +4,30 @@ import { Link } from "react-router-dom";
 function Project({ id, title, deployedLink, githubLink, image }) {
   return (
     <div>
-      <h3>{title}</h3>
-      <img src={image} alt={`${title} Preview`} />
-      <p>
-        <a href={deployedLink} target="_blank" rel="noopener noreferrer">
-          View Deployed
-        </a>
-      </p>
-      <p>
-        <a href={githubLink} target="_blank" rel="noopener noreferrer">
-          GitHub Repository
-        </a>
-      </p>
+      {/* //   <h3>{title}</h3>
+    //   <img src={image} alt={`${title} Preview`} />
+    //   <p>
+    //     <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+    //       View Deployed
+    //     </a>
+    //   </p>
+    //   <p>
+    //     <a href={githubLink} target="_blank" rel="noopener noreferrer">
+    //       GitHub Repository
+    //     </a>
+    //   </p> */}
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card">
+            <div className={`card-body ${image}`}>
+              <a href={deployedLink} className="btn btn-primary codeWars">
+                <h4 className="card-title">{title}</h4>
+                {/* <p className="card-text">Kata</p> */}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <Link to={`/work/${id}`}>Details</Link>
     </div>
   );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,11 +4,15 @@ import Project from "./Project";
 
 function Work() {
   return (
-    <div>
-      <h2>Project Gallery</h2>
-      {projectsData.map((project) => (
-        <Project key={project.id} {...project} />
-      ))}
+    <div className="raw">
+      <h2 id="scrollspyHeading2" class="fs-2 col-2 text-end">
+        Project Gallery
+      </h2>
+      <div className="col-10 border-start border-dark border-4">
+        {projectsData.map((project) => (
+          <Project key={project.id} {...project} />
+        ))}
+      </div>
     </div>
   );
 }
