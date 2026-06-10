83194cd091028e00de6e91c93fe636e5f483dea5
Enky <enkh6at@yahoo.com>
2026-06-10 12:51:37 +0100

    Fix home page layout bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const handleGlowMove = (event) => {
+    const link = event.currentTarget;
+    const rect = link.getBoundingClientRect();
+
+    link.style.setProperty("--cursor-x", `${event.clientX - rect.left}px`);
+    link.style.setProperty("--cursor-y", `${event.clientY - rect.top}px`);
+  };
+
+  const getNavLinkClass = ({ isActive }) =>
+    `nav-link navbar-glow-link${isActive ? " active" : ""}`;
+
   return (
     <nav id="navbar-example2" className="navbar bg-body-tertiary px-3 fs-4">
         <NavLink to="/" className="navbar-brand fs-3">
@@ -9,22 +20,22 @@ const Navbar = () => {
         </NavLink>
         <ul className="nav nav-pills">
           <li className="nav-item">
-            <NavLink to="/" className="nav-link">
+            <NavLink to="/" className={getNavLinkClass} onMouseMove={handleGlowMove}>
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/about" className="nav-link">
+            <NavLink to="/about" className={getNavLinkClass} onMouseMove={handleGlowMove}>
               About Me
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/work" className="nav-link">
+            <NavLink to="/work" className={getNavLinkClass} onMouseMove={handleGlowMove}>
               Work
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/contact" className="nav-link">
+            <NavLink to="/contact" className={getNavLinkClass} onMouseMove={handleGlowMove}>
               Contact Me
             </NavLink>
           </li>
