a63b93270e23aeb802487c5ee4bd75172b0821a6
Enky <enkh6at@yahoo.com>
2026-06-10 12:34:01 +0100

    Fix home page layout bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,49 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
 const Home = () => {
   const [isHovering, setIsHovering] = useState(false);
   const imagePath = `${import.meta.env.BASE_URL}matrix.jpeg`;
 
-  const handleMouseEnter = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-  };
   return (
-    <div className="row">
+    <main className="home-page">
       <div
-        className="jumbotron jumbotron-fluid"
+        className="jumbotron jumbotron-fluid home-hero"
         id="hero-bg"
         style={{ backgroundImage: `url(${imagePath})` }}
       >
-        <div className="container">
-          <h6 className="display-4">Create Mindset to Learn!</h6>
+        <div className="container home-hero-content">
+          <h1 className="display-4 home-title">Create Mindset to Learn!</h1>
         </div>
-        <div className="fixed-bottom">
-          <ul className="fs-4 nav justify-content-center justify-content-around align-content-center fixed-bottom">
+
+        <nav className="home-contact-bar" aria-label="Social links">
+          <ul className="fs-4 nav home-contact-nav">
             <li className="nav-item">
               <a
                 className="nav-link"
                 href="https://www.linkedin.com/in/enkhbat-baatar-521a3890"
+                target="_blank"
+                rel="noreferrer"
               >
-                Linkedin
+                LinkedIn
               </a>
             </li>
             <li
-              className="nav-item phone-nav-item"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
+              className="nav-item home-phone-item"
+              onMouseEnter={() => setIsHovering(true)}
+              onMouseLeave={() => setIsHovering(false)}
+              onFocus={() => setIsHovering(true)}
+              onBlur={() => setIsHovering(false)}
             >
-              <button className="nav-link btn btn-link p-0 border-0">Phone</button>
-                {isHovering && (
-                  <span className="phone-tooltip">123456789</span>
-                )}
+              <button
+                className="nav-link home-phone-button"
+                type="button"
+                aria-expanded={isHovering}
+                onClick={() => setIsHovering(true)}
+              >
+                Phone
+              </button>
+              {isHovering && <span className="phone-tooltip">123456789</span>}
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/contact">
@@ -47,14 +51,19 @@ const Home = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="https://www.instagram.com">
+              <a
+                className="nav-link"
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Instagram
               </a>
             </li>
           </ul>
-        </div>
+        </nav>
       </div>
-    </div>
+    </main>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './css/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/style.css'
 import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
